2a57f7c5d9b76d121510eca20a6f13690c58e6da
Emanuel Nóbrega <egn1995@hotmail.com>
2021-08-27 17:29:21 -0300

    Primeiro Commit

---
+++ src/app/shared/model/USUARIOS.ts
@@ -0,0 +1,17 @@
+export const USUARIOS = [
+  {
+    nome: 'Emanuel',
+    cpf: '123456789',
+    idade: 25
+  },
+  {
+    nome: 'Nóbrega',
+    cpf: '123456789',
+    idade: 52
+  },
+  {
+    nome: 'Henrique',
+    cpf: '123456789',
+    idade: 25
+  }
+];
