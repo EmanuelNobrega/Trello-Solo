049b779e812dc66632bb64d8b1abb81655379b03
Emanuel Nóbrega <39737266+EmanuelNobrega@users.noreply.github.com>
2021-08-29 21:34:25 -0300

    Update signup.module.ts

--- src/app/components/signup/signup.module.ts
+++ src/app/components/signup/signup.module.ts
@@ -1,15 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SignupComponent } from './signup.component';
-import { FormsModule } from '@angular/forms';
-import { MatCard } from '@angular/material/card';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../shared/model/user';
 
-@NgModule({
-  declarations: [SignupComponent],
-  imports: [
-    FormsModule,
-    CommonModule,
-    MatCard
-  ]
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
 })
-export class SignupModule { }
+export class SignupComponent implements OnInit {
+  user: User;
+
+  constructor(
+    private userService: UserService,
+    private currentRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.user = new User();
+  }
+
+  ngOnInit(): void {}
+
+  insertUser(): void {
+    this.userService.create(this.user).subscribe(ret => {
+      // navega para a página inicial após cadastro bem sucedido
+      this.router.navigate(['/']);
+    });
+  }
+}
